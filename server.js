a0a753f0e64032aacf735cfcb0327f70d047b8b1
Lisandro <47686025+Slisandro@users.noreply.github.com>
2020-12-23 19:40:15 -0300

    Update server.js

--- server.js
+++ server.js
@@ -43,5 +43,5 @@ app.delete("/api/products/:id", async (req, res) => {
     res.send(deletedProduct)
 })
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log("serve at http://localhost/5000"));
+// const port = process.env.PORT || 5000;
+// app.listen(port, () => console.log("serve at http://localhost/5000"));
